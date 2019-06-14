bc6605d51d58eb6fb2fdd8448c12ea40f5150c59
villegas-06 <villegas4006@gmail.com>
2019-06-14 11:05:08 -0500

    condicionales 2

---
+++ condicionales-2/index.js
@@ -0,0 +1,9 @@
+var metros = 2.0
+
+if(metros % 2 == 0){
+var kilometros = metros / 1000; 
+console.log("Los Kilómetros son: " + kilometros);   
+}else{
+    var decimentros = metros*10;
+    console.log("Los Decímetros son: " + decimentros);
+}
