1ffd80c27bd5addca9b69ca6e853e38c66b6d88f
villegas-06 <villegas4006@gmail.com>
2019-06-14 14:43:05 -0500

    condicionales 3

--- condicionales-3/index.js
+++ condicionales-3/index.js
@@ -1,2 +1,14 @@
 var nombre = "John";
 var edad = 15;
+
+if (edad >= 18){
+    var dias = edad * 365;
+    console.log(nombre + " " + "ha vivido: " + dias );    
+}else
+if(edad % 2 == 0){
+    var horas = edad *365*24
+    console.log("Ha vivido: " + horas);
+}else{
+    console.log(nombre + " " + "Hola")
+}
+  
