30f2e5d9ecb0b38d70e95f8bcd484e6294561014
villegas-06 <villegas4006@gmail.com>
2019-06-15 10:34:44 -0500

    subprograma 1

---
+++ sub_programas-1/index.js
@@ -0,0 +1,32 @@
+var x = 0, y = 0;
+
+function Pn_subp(){
+    var z = 0;
+    z = 2;
+    x = 3;
+    y = 2;
+
+    console.log(z,x,y);
+    MMM(x,z);
+    console.log(x,y,z);
+    
+}
+
+Pn_subp();
+
+function MMM(){
+var A = 3;
+var B = 2;
+var z = 0;
+
+var x = A + B;
+B = B * 2;
+z = x + B;
+y = y + x *B;
+
+console.log(x,B,z);
+
+}
+
+MMM();
+
